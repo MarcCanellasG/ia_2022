4812e4b4cf256334b20a2004a72d78ccbd89f0e5
MarcCanellasG <marccg8@gmail.com>
2022-10-11 18:00:52 +0200

    a

--- quiques/agent_amplada.py
+++ quiques/agent_amplada.py
@@ -9,19 +9,29 @@ class BarcaAmplada(Barca):
         self.__oberts = None
         self.__tancats = None
         self.__accions = None
-        self.__solucio=None
+        self.__solucio = []
 
     def actua(self, percep: entorn.Percepcio) -> entorn.Accio | tuple[entorn.Accio, object]:
-        inicial=Estat[percep.to_dict()]
-        OBERT=[inicial]
-        TANCAT=[]
+        inicial = Estat(percep.to_dict())
+        self.__oberts = []
+        self.__tancats = []
 
-        while OBERT:
-            actual=OBERT.pop()
+        self.__oberts.append(inicial)
+
+        while self.__oberts:
+            actual = self.__oberts.pop(0)
+            print(actual)
             if Estat.es_meta(actual):
-                pass
+                for actual.pare in self.__oberts:
+                    if actual.pare not in self.__tancats:
+                        self.solucio.append(actual.pare)
+                        actual=actual.pare
+
+                return tuple(reversed(self.__solucio))
+
             else:
-                successor = actual.genera_fill()
-                TANCAT.append(actual)
+                successors = actual.genera_fill()
+                self.__tancats.append(actual)
+                self.__oberts.append(successors)
 
         pass
